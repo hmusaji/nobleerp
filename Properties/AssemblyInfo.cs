0c10a7effc32ac0c51b5541217661303339ee2e3
hmusaji <husain.musaji@gmail.com>
2023-09-27 15:26:47 +0300

    x
    x

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,36 +1,36 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-
-// Review the values of the assembly attributes
-
-
-[assembly: AssemblyTitle("Xtreme Innova")]
-[assembly: AssemblyDescription("Xtreme Innova")]
-[assembly: AssemblyCompany("Hakimi Solutions")]
-[assembly: AssemblyProduct("Xtreme 4.0")]
-[assembly: AssemblyCopyright("The software product is protected by copyright laws and international copyright treaties, as well as other intellectual property laws and treaties. The software product is licensed, not sold.")]
-[assembly: AssemblyTrademark("Xtreme")]
+[assembly: AssemblyTitle("Xtreme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xtreme")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86ed9976-88dd-42ba-a8f2-f84aa1bff80c")]
+
 // Version information for an assembly consists of the following four values:
-
-//  Major Version
-//  Minor Version
-//  Build Number
-//  Revision
-
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion=("1.0.*")]
-
-
-[assembly: AssemblyVersion("6.0.0")]
-
-
-[assembly: ClassInterface(ClassInterfaceType.AutoDual)]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- packages/C1.Win.C1DX.4.5.20232.611/docs/readme.md
+++
@@ -1,8 +0,0 @@
-## About
-
-Provides DirectX rendering support for ComponentOne WinForms controls.
-
-## Resources
-
-* [Documentation](https://www.grapecity.com/componentone/docs/win/online-bitmap/APIReference.html#)
-* [Licensing](https://www.grapecity.com/componentone/licensing)
